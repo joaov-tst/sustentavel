cc6416271bff57c29b5c1360c094dfa4bece355a
joaov-tst <110254205+joaov-tst@users.noreply.github.com>
2025-12-15 23:50:32 -0300

    Update allowed origins in CorsConfig

--- src/main/java/com/pireco/sustentavel/config/cors/CorsConfig.java
+++ src/main/java/com/pireco/sustentavel/config/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Mapeia todos os endpoints
-                .allowedOrigins("http://localhost:4200", "http://localhost:5173", "https://vercel-ti-k7yc-5vpw9f9c0-gabriel-azevedos-projects-16bc0d72.vercel.app")
+                .allowedOrigins("http://localhost:4200", "https://vercel-ti-k7yc.vercel.app", "https://vercel-ti-k7yc-5vpw9f9c0-gabriel-azevedos-projects-16bc0d72.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite credenciais
